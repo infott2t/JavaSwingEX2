448d15c42a8a3a42a509a8476b37cc42cae130e8
HYEONIL CHOI <tt2t2am1118@naver.com>
2024-07-22 11:58:55 +0900

    Update Util.java
    설명추가. 구글 메일보내기 . SMTP서버 사용방법. 블로그 링크.

--- src/main/java/com/example/demo2/util/Util.java
+++ src/main/java/com/example/demo2/util/Util.java
@@ -21,6 +21,7 @@ public class Util {
 
 
     // 메일 발송용
+    // https://blog.naver.com/tt2t2am1118/223520863617 참조해보세요. [프로그래밍] 구글 메일 설정~. SMTP 서버 사용해보기. 메일~.
     static String sendMailer = "";  //이 부분을 수정하면 보낸 메일을 보낼 수 있습니다. ex) 구글 메일형식. example@gmail.com
     static String password = "";  //메일보낼 때에 암호입력. 메일보내기를 공부해보세요~. 작동 됨니다.
 
